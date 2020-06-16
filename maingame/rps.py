6f8f1c6f3ea3921dbbb18a78ef03aafa9d867f46
delivey <deliveydev@gmail.com>
2020-06-16 10:07:47 +0300

    added playing again, pls test
    ^

--- maingame/rps.py
+++ maingame/rps.py
@@ -4,47 +4,52 @@
 #v 2.0: Will add play again option. Also maybe runtime.
 
 #importing random for random.choice()
-import random
-# Notifying user with the credits and stuff
-print("Hello, and welcome to RockPaperScissors, credits to @abhishekshahane, v1.0")
-print("You are currently using v1.0. This version will be updated soon to v2.0!")
-print("Use 'git pull' for updating v1.0 to v2.0 as soon as it comes out.")
-# List, input and random.choice(list)
-list = ["Rock","Paper", "Scissors"]
-a=input("Enter a option(First letter in caps):  ")
-b = random.choice(list)
-#Making function errorchecking
-if (a not in list):
-    def errorchecking(a):
-        print("Please enter a valid value.")
-        print("If you want to play again, run python rps.py")
-        exit()
-#Making main game function
-elif (a in list):
-    def rpsmain(a,b):
-        b = random.choice(list)
-        if b=="Scissors" and a=="Paper":
-            print("Computer won, now exiting")
+def play_rps():
+
+    import random
+    # Notifying user with the credits and stuff
+    print("Hello, and welcome to RockPaperScissors, credits to @abhishekshahane, v1.0")
+    print("You are currently using v1.0. This version will be updated soon to v2.0!")
+    print("Use 'git pull' for updating v1.0 to v2.0 as soon as it comes out.")
+    # List, input and random.choice(list)
+    list = ["Rock","Paper", "Scissors"]
+    a=input("Enter a option(First letter in caps):  ")
+    b = random.choice(list)
+    #Making function errorcheckin
+    if (a not in list):
+        def errorchecking(a):
+            print("Please enter a valid value.")
+            print("If you want to play again, run python rps.py")
             exit()
-        if b=="Paper" and a=="Rock":
-            print("Computer won, now exiting")
-            exit()
-        if b=="Rock" and a=="Scissors":
-            print("Computer won, now exiting")
-            exit()
-        if a=="Scissors" and b=="Paper":
-            print("You won, now exiting")
-            exit()
-        if a=="Paper" and b=="Rock":
-            print("You won, now exiting")
-            exit()
-        if a=="Rock" and b=="Scissors":
-            print("You won, now exiting")
-            exit()
+        errorchecking(a)
+
+    # Making main game function
+    elif (a in list):
+        def rpsmain(a,b):
+            b = random.choice(list)
+            if b=="Scissors" and a=="Paper":
+                print("Computer won, now exiting")
+            elif b=="Paper" and a=="Rock":
+                print("Computer won, now exiting")
+            elif b=="Rock" and a=="Scissors":
+                print("Computer won, now exiting")
+            elif a=="Scissors" and b=="Paper":
+                print("You won, now exiting")
+            elif a=="Paper" and b=="Rock":
+                print("You won, now exiting")
+            elif a=="Rock" and b=="Scissors":
+                print("You won, now exiting")
+            elif a==b:
+                print("Tie")
+
+        rpsmain(a,b)
+
+        playA = input("Do you want to play again (y/n) ?").lower()
+        if playA == "y":
+            play_rps()
         else:
-            print("Tie, now exiting")
             exit()
+
+play_rps()
 # Calling them at the end of this process
-rpsmain(a,b)
-errorchecking(a)
-     
+            
